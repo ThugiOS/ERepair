0778847b340e6c187f9f7b95fe18675140ef23df
catthug92 <itnikitins@gmail.com>
2023-06-01 15:37:21 +0300

    3 screen done

--- ERepair/HomeScreen/MapViewController.swift
+++ ERepair/HomeScreen/MapViewController.swift
@@ -5,71 +5,6 @@
 //  Created by Никитин Артем on 30.03.23.
 //
 
-//import UIKit
-//import CoreLocation
-//import MapKit
-//
-//class MapViewController: UIViewController {
-//
-//    // MARK: - Variables
-//    private let locationManager = CLLocationManager()
-//
-//    // MARK: - UI Components
-//    let mapView = MKMapView()
-//    private let label: UILabel = {
-//        let label = UILabel()
-//        label.textColor = .label
-//        label.textAlignment = .center
-//        label.font = .systemFont(ofSize: 16, weight: .regular)
-//        label.text = "Title"
-//        return label
-//    }()
-//
-//
-//    // MARK: - LifeCycle
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupUI()
-//
-//        mapView.showsUserLocation = true
-//
-//        locationManager.requestWhenInUseAuthorization()
-//
-//        let annotation = MKPointAnnotation()
-//        annotation.coordinate = CLLocationCoordinate2D(latitude: 53.908933, longitude: 30.333573)
-//        annotation.title = "Мастерская"
-//        annotation.subtitle = "Вход со двора"
-//
-//        mapView.addAnnotation(annotation)
-//    }
-//
-//    // MARK: - UI Setup
-//    private func setupUI() {
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        mapView.translatesAutoresizingMaskIntoConstraints = false
-//
-//
-//        view.addSubview(mapView)
-//        mapView.addSubview(label)
-//
-//        NSLayoutConstraint.activate([
-//            mapView.topAnchor.constraint(equalTo: view.topAnchor),
-//            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//
-//            label.centerXAnchor.constraint(equalTo: mapView.centerXAnchor),
-//            label.centerYAnchor.constraint(equalTo: mapView.centerYAnchor),
-//        ])
-//    }
-//
-//}
-//
-//extension MapViewController: MKMapViewDelegate {
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        return nil
-//    }
-//}
 
 import UIKit
 import CoreLocation
@@ -77,8 +12,15 @@ import MapKit
 
 class MapViewController: UIViewController {
 
-    // MARK: - Variables
     private let locationManager = CLLocationManager()
+    
+    private let shopCoordinate = CLLocationCoordinate2D(latitude: 53.908933, longitude: 30.333573)
+    private let shopPin: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.title = "пр-т Мира 27"
+        annotation.subtitle = "Мастерская"
+        return annotation
+    }()
 
     // MARK: - UI Components
     private let mapView = MKMapView()
@@ -86,8 +28,9 @@ class MapViewController: UIViewController {
         let label = UILabel()
         label.textColor = .label
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 16, weight: .regular)
-        label.text = "Title"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.text = "г. Могилёв, пр-т Мира 27, вход со двора\nПн-Пт 9-18"
+        label.numberOfLines = 2
         label.isHidden = true
         return label
     }()
@@ -95,30 +38,56 @@ class MapViewController: UIViewController {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.image = UIImage(named: "shop")
-//        iv.tintColor = .white
         iv.isHidden = true
         iv.layer.cornerRadius = 20
         return iv
+    }()
+    private let showShopPhotoButton: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        button.setImage(UIImage(systemName: "photo.fill"), for: .normal)
+        return button
+    }()
+    
+    private let showLocation: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        button.setImage(UIImage(systemName: "location"), for: .normal)
+        return button
+    }()
+    
+    private let showShopLocation: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        button.setImage(UIImage(systemName: "scooter"), for: .normal)
+        return button
     }()
 
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        shopPin.coordinate = shopCoordinate
 
         mapView.showsUserLocation = true
+        mapView.addAnnotation(shopPin)
+        mapView.setRegion(MKCoordinateRegion(center: shopCoordinate,
+                                             span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                    longitudeDelta: 0.01)),
+                          animated: true
+        )
 
-        locationManager.requestWhenInUseAuthorization()
-
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 53.908933, longitude: 30.333573)
-        annotation.title = "Мастерская"
-        annotation.subtitle = "Вход со двора"
-
-        mapView.addAnnotation(annotation)
-
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped(_:)))
-        mapView.addGestureRecognizer(tapGesture)
+        let tapMap = UITapGestureRecognizer(target: self, action: #selector(mapTapped(_:)))
+        self.mapView.addGestureRecognizer(tapMap)
+        self.showShopPhotoButton.addTarget(self, action: #selector(didTapShowPhoto), for: .touchUpInside)
+        self.showLocation.addTarget(self, action: #selector(didTapShowYourLocation), for: .touchUpInside)
+        self.showShopLocation.addTarget(self, action: #selector(didTapShopLocation), for: .touchUpInside)
     }
 
     // MARK: - UI Setup
@@ -127,12 +96,16 @@ class MapViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         mapView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
-
+        showShopPhotoButton.translatesAutoresizingMaskIntoConstraints = false
+        showLocation.translatesAutoresizingMaskIntoConstraints = false
+        showShopLocation.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(mapView)
-        mapView.addSubview(label)
-        mapView.addSubview(imageView)
-
+        view.addSubview(label)
+        view.addSubview(imageView)
+        view.addSubview(showShopPhotoButton)
+        view.addSubview(showLocation)
+        view.addSubview(showShopLocation)
 
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -144,19 +117,65 @@ class MapViewController: UIViewController {
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.widthAnchor.constraint(equalToConstant: 330.0),
             imageView.heightAnchor.constraint(equalToConstant: 150.0),
+            
+            label.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: -10.0),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            showShopPhotoButton.widthAnchor.constraint(equalToConstant: 50.0),
+            showShopPhotoButton.heightAnchor.constraint(equalToConstant: 50.0),
+            showShopPhotoButton.bottomAnchor.constraint(equalTo: label.topAnchor, constant: -30.0),
+            showShopPhotoButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20.0),
+            
+            showLocation.centerXAnchor.constraint(equalTo: showShopPhotoButton.centerXAnchor),
+            showLocation.bottomAnchor.constraint(equalTo: showShopPhotoButton.topAnchor, constant: -10.0),
+            showLocation.widthAnchor.constraint(equalToConstant: 50.0),
+            showLocation.heightAnchor.constraint(equalToConstant: 50.0),
+            
+            showShopLocation.centerXAnchor.constraint(equalTo: showShopPhotoButton.centerXAnchor),
+            showShopLocation.bottomAnchor.constraint(equalTo: showLocation.topAnchor, constant: -10.0),
+            showShopLocation.widthAnchor.constraint(equalToConstant: 50.0),
+            showShopLocation.heightAnchor.constraint(equalToConstant: 50.0),
         ])
     }
 
-    // MARK: - Gesture Recognizer
-    @objc private func mapViewTapped(_ gestureRecognizer: UITapGestureRecognizer) {
-        label.isHidden = false
-        imageView.isHidden = false
+    // MARK: - Selectors
+    @objc
+    private func mapTapped(_ gestureRecognizer: UITapGestureRecognizer) {
+            imageView.isHidden = true
+            label.isHidden = true
     }
-}
-
-extension MapViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        return nil
+    
+    @objc
+    private func didTapShowPhoto() {
+        if imageView.isHidden {
+            imageView.isHidden = false
+            label.isHidden = false
+        } else {
+            imageView.isHidden = true
+            label.isHidden = true
+        }
+    }
+    
+    @objc
+    private func didTapShowYourLocation() {
+        let latitudeLocation = Double(locationManager.location?.coordinate.latitude ?? 0.0)
+        let longitudeLocation = Double(locationManager.location?.coordinate.longitude ?? 0.0)
+        
+        mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitudeLocation,
+                                                                            longitude: longitudeLocation),
+                                             span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                    longitudeDelta: 0.01)),
+                          animated: true
+        )
+    }
+    
+    @objc
+    private func didTapShopLocation() {
+        mapView.setRegion(MKCoordinateRegion(center: shopCoordinate,
+                                             span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                    longitudeDelta: 0.01)),
+                          animated: true
+        )
     }
 }
 
